cdfd524a3cf97c403e3d899a6ec497b051d7176e
zalachenka <saliou.armony@gmail.com>
2019-05-07 10:25:15 +0200

    navbar

---
+++ app/controllers/event_controller.rb
@@ -0,0 +1,6 @@
+class EventController < ApplicationController
+
+  def index
+  		@event = Event.all
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'event/index'
 	root 'static_page#index'
   get 'static_page/index'
   get 'static_page/secret'
   devise_for :users
+  resources :event
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
